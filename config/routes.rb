f60a7f4677138ff78cb20b57e7fd96be8f0fada9
taro.kusachi <chuki.s21@gmail.com>
2019-10-02 06:26:19 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'toppages/index'
+  root to: 'toppages#index'
   
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, only: [:new, :create]
   resources :tasks
 end
